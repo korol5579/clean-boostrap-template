d60892859e35fd161dbf0da61cd96f413f7e8b1d
Василий <naukomania@gmail.com>
2017-08-18 17:31:29 +0300

    carousel fix

--- js/script.js
+++ js/script.js
@@ -39,10 +39,11 @@ $(document).ready(function() {
 
     // Owl carousel
     $('.owl-carousel').owlCarousel({
+        // autoWidth:true,
         navigation: false, // Show next and prev buttons
         slideSpeed: 300,
         paginationSpeed: 400,
-        singleItem: true
+        singleItem: true,
     });
 
 });
